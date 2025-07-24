dadc807de92d6ba8be62f399ec3dc66c673773da
rake.huang <rake.huang@titansoft.com.sg>
2025-07-24 23:36:28 +0800

    feat: query budget

--- WebApplication2.Tests/BudgetServiceTests.cs
+++ WebApplication2.Tests/BudgetServiceTests.cs
@@ -37,6 +37,66 @@ public class BudgetServiceTests
         amount.Should().Be(160);
     }
 
+    [Test]
+    public void query_budget_when_get_cross_month()
+    {
+        GivenBudget(
+            CreateBudget("202507", 620)
+            , CreateBudget("202506", 300)
+        );
+        var amount = _budgetService.Query(new DateTime(2025, 06, 01), new DateTime(2025, 07, 31));
+        amount.Should().Be(920);
+    }
+
+    [Test]
+    public void query_budget_when_get_partial_cross_month()
+    {
+        GivenBudget(
+            CreateBudget("202506", 300),
+            CreateBudget("202507", 310)
+        );
+        var amount = _budgetService.Query(new DateTime(2025, 06, 21), new DateTime(2025, 07, 10));
+        amount.Should().Be(200);
+    }
+
+    [Test]
+    public void query_budget_when_get_cross_year()
+    {
+        GivenBudget(
+            CreateBudget("202412", 310),
+            CreateBudget("202501", 310)
+        );
+        var amount = _budgetService.Query(new DateTime(2024, 12, 01), new DateTime(2025, 01, 31));
+        amount.Should().Be(620);
+    }
+
+    [Test]
+    public void query_budget_when_get_partial_cross_year()
+    {
+        GivenBudget(
+            CreateBudget("202412", 310),
+            CreateBudget("202501", 310)
+        );
+        var amount = _budgetService.Query(new DateTime(2024, 12, 31), new DateTime(2025, 01, 01));
+        amount.Should().Be(10 + 10);
+    }
+
+    [Test]
+    public void query_budget_with_no_budget_data()
+    {
+        GivenBudget();
+        var amount = _budgetService.Query(new DateTime(2025, 01, 01), new DateTime(2025, 01, 31));
+        amount.Should().Be(0);
+    }
+
+    [Test]
+    public void query_budget_with_invalid_date_range()
+    {
+        GivenBudget(CreateBudget("202501", 310));
+        var amount = _budgetService.Query(new DateTime(2025, 01, 31), new DateTime(2025, 01, 01));
+        amount.Should().Be(0);
+    }
+
 
     private static Budget CreateBudget(string yearMonth, int amount)
     {
--- WebApplication2/Services/BudgetService.cs
+++ WebApplication2/Services/BudgetService.cs
@@ -2,17 +2,41 @@ using WebApplication2.Repositories;
 
 namespace WebApplication2.Services;
 
+
 public class BudgetService(IBudgetRepo budgetRepo)
 {
     public decimal Query(DateTime startTime, DateTime endTime)
     {
-        var budgets = budgetRepo.GetAll();
-        var startYearMonth = $"{startTime:yyyyMM}";
-        var budget = budgets.FirstOrDefault(e => e.YearMonth == startYearMonth);
-        
-        var daysInMonth = DateTime.DaysInMonth(startTime.Year, startTime.Month);
-        var effectiveDays = (endTime - startTime).Days + 1;
-        
-        return Math.Round((decimal)(budget.Amount * effectiveDays / daysInMonth), 0);
+        if (startTime > endTime)
+        {
+            return 0;
+        }
+
+        var budgets = budgetRepo.GetAll().ToDictionary(b => b.YearMonth);
+        decimal totalAmount = 0;
+
+        var startMonth = new DateTime(startTime.Year, startTime.Month, 1);
+        var endMonth = new DateTime(endTime.Year, endTime.Month, 1);
+
+        var currentMonth = startMonth;
+        while (currentMonth <= endMonth)
+        {
+            var yearMonthStr = currentMonth.ToString("yyyyMM");
+            if (budgets.TryGetValue(yearMonthStr, out var budget))
+            {
+                var daysInMonth = DateTime.DaysInMonth(currentMonth.Year, currentMonth.Month);
+                var dailyAmount = (decimal)budget.Amount / daysInMonth;
+
+                var startDay = (currentMonth == startMonth) ? startTime.Day : 1;
+                var endDay = (currentMonth == endMonth) ? endTime.Day : daysInMonth;
+
+                var effectiveDays = endDay - startDay + 1;
+                totalAmount += dailyAmount * effectiveDays;
+            }
+
+            currentMonth = currentMonth.AddMonths(1);
+        }
+
+        return totalAmount;
     }
 }
